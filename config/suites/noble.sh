dec76e75b99dbe38b6ceeb6e563c6bdf9e995690
wanpublic <wanpublic@example.com>
2026-01-16 14:33:27 +0000

    chore: update KERNEL_BRANCH to noble for initial build setup

--- config/suites/noble.sh
+++ config/suites/noble.sh
@@ -3,8 +3,9 @@
 export RELASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
 export RELASE_VERSION="24.04"
 
+# 2026-01-16: 将 KERNEL_BRANCH 修改为 noble，初始环境搭建
 export KERNEL_REPO="https://github.com/WanPublic/linux-rockchip.git"
-export KERNEL_BRANCH="orange-pi-5-plus-imx415-1080p-90fps"
+export KERNEL_BRANCH="noble"
 export KERNEL_FLAVOR="rockchip"
 
 export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"
