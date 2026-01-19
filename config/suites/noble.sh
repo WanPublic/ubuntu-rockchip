0e2710fc662ce7b86334ee5c159ac8b57a9f1ffe
wanpublictech <wanpublictech@gmail.com>
2026-01-20 00:08:40 +0800

    chore: switch kernel branch to rock-5b-plus-imx415-4k-65fps-singleisp

--- config/suites/noble.sh
+++ config/suites/noble.sh
@@ -3,6 +3,7 @@
 export RELASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
 export RELASE_VERSION="24.04"
 
+# 2025-01-20: Updated kernel branch to rock-5b-plus-imx415-4k-65fps-singleisp - Antigravity
 # 2025-01-18: Updated kernel repo and branch to match local checkout - Antigravity
 export KERNEL_REPO="https://github.com/WanPublic/linux-rockchip.git"
 export KERNEL_BRANCH="rock-5b-plus-imx415-4k-65fps-singleisp"
