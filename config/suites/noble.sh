d9a1368342e0313ca9ad70c138e4e0c01564a29b
wanpublic <wanpublic@example.com>
2026-01-30 09:13:43 +0000

    config: noble: switch to orange-pi-5-plus-imx415-1080P-90ps-singleisp branch

--- config/suites/noble.sh
+++ config/suites/noble.sh
@@ -9,8 +9,8 @@ export RELASE_VERSION="24.04"
 # 2026-01-16: 切换到 orange-pi-5-plus-imx415-4k-90fps-dualisp 分支进行 4K 90fps dual isp 开发
 export KERNEL_REPO="https://github.com/WanPublic/linux-rockchip.git"
 # 2026-01-17: 切换到 orange-pi-5-plus-imx415-4k-90fps-singleisp 分支进行内核编译
-# 2026-01-19: 切换到 orange-pi-5-plus-imx415-4k-65fps-singleisp 分支以解决 ISP 分辨率问题
-export KERNEL_BRANCH="orange-pi-5-plus-imx415-4k-65fps-singleisp"
+# 2026-01-30: 切换到 orange-pi-5-plus-imx415-1080P-90ps-singleisp 分支进行 1080p 90fps 编译测试
+export KERNEL_BRANCH="orange-pi-5-plus-imx415-1080P-90ps-singleisp"
 export KERNEL_FLAVOR="rockchip"
 
 export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"
